a930f6547e03926e1c3cab48f96d5255def00a96
pq@github <github@paulquinn.com>
2014-09-24 22:42:56 -0700

    api_v1: added redirect from /actors/ to /contacts/

--- minezy_api/minezy_api/api_v1/api_contacts.py
+++ minezy_api/minezy_api/api_v1/api_contacts.py
@@ -1,9 +1,8 @@
 from minezy_api import app
 from minezy_api.api_v1.api_common import support_jsonp, query_params
 from minezy_api.api_v1.query_contacts import query_contacts
-from flask import jsonify, request 
+from flask import jsonify, request, redirect, url_for
 from __builtin__ import str
-
 
 
 @app.route('/1/contacts/', methods=['GET'])
@@ -21,3 +20,13 @@ def contacts_count():
     resp = query_contacts(params, countResults=True)
     return jsonify( { 'contacts' : resp } )
     
+
+@app.route('/1/actors/', methods=['GET'])
+@support_jsonp
+def actors_deprecated():
+    return redirect(url_for('contacts', **request.args))
+
+@app.route('/1/actors/count/', methods=['GET'])
+@support_jsonp
+def actors_count_deprecated():
+    return redirect(url_for('contacts_count', **request.args))
