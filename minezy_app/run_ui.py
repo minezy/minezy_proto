074ff76ab505cb76e585bf3659e96a6a739192d0
pq@github <github@paulquinn.com>
2014-10-30 21:12:50 -0700

    config params for run_ui

--- minezy_app/run_ui.py
+++ minezy_app/run_ui.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python
+import sys
 from ui import app
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True, threaded=True, port=8080)
+    host = "127.0.0.1"
+    port = 8080
+    bDebug = True
+    
+    if len(sys.argv) > 1:
+        host = str(sys.argv[1])
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
+    if len(sys.argv) > 3:
+        bDebug = bool(sys.argv[3])
+        
+    app.run(host=host, port=port, debug=bDebug, use_reloader=bDebug, threaded=True)
 
 
