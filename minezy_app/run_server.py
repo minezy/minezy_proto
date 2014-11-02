f308041522541d829ccf39b445686a35b8da6b37
pq@github <github@paulquinn.com>
2014-11-01 18:07:49 -0700

    runserver with configurable params

--- minezy_app/run_server.py
+++ minezy_app/run_server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import sys
 from minezy_api import neo4j_conn
 from minezy_api import app
 
@@ -8,6 +9,18 @@ if __name__ == '__main__':
 
     neo4j_conn.connect()
 
-    app.run(debug=True, use_reloader=True, threaded=True)
+    host = "127.0.0.1"
+    port = 5000
+    bDebug = False
+    
+    if len(sys.argv) > 1:
+        host = str(sys.argv[1])
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
+    if len(sys.argv) > 3:
+        if int(sys.argv[3]):
+            bDebug = True 
+
+    app.run(host=host, port=port, debug=bDebug, use_reloader=bDebug, threaded=True)
 
 
