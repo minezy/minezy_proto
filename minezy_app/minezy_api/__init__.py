a5f0226c4814515c8db0a6c62bc767d5e577456a
pq@github <github@paulquinn.com>
2014-11-16 10:39:05 -0800

    flask memcache setup

--- minezy_app/minezy_api/__init__.py
+++ minezy_app/minezy_api/__init__.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 cors = CORS(app)
 
 try:
-    sys.stdout.write("Connect to Memcached... ")
-    app.cache = Cache(app,config={'CACHE_TYPE': 'memcached'})
+    sys.stdout.write("Flask-Cache 'memcached'... ")
+    app.cache = Cache(app,config={'CACHE_TYPE': 'memcached', 'CACHE_MEMCACHED_SERVERS': ['127.0.0.1:11211']})
     sys.stdout.write("OK\n")
 except:
     sys.stdout.write("Failed\n")
-    sys.stdout.write("Using 'Simple' Flask-Cache.\n")
+    sys.stdout.write("Flask-Cache 'simple'... ")
     app.cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+    sys.stdout.write("OK\n")
     pass
 
 import api_root
