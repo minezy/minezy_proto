2bb749aee5277d0e6a0de847f048466e485c8742
pq@github <github@paulquinn.com>
2014-11-16 10:25:38 -0800

    flask-cache: attempt to connect to memcached, fallback to simple

--- minezy_app/minezy_api/__init__.py
+++ minezy_app/minezy_api/__init__.py
@@ -1,10 +1,20 @@
+import sys
 from flask import Flask
 from flask.ext.cors import CORS
 from flask.ext.cache import Cache
 
 app = Flask(__name__)
 cors = CORS(app)
-app.cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+
+try:
+    sys.stdout.write("Connect to Memcached... ")
+    app.cache = Cache(app,config={'CACHE_TYPE': 'memcached'})
+    sys.stdout.write("OK\n")
+except:
+    sys.stdout.write("Failed\n")
+    sys.stdout.write("Using 'Simple' Flask-Cache.\n")
+    app.cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+    pass
 
 import api_root
 import api_v1
